34f03760ff1f11797e9937fe1bb7c18afc4b59c0
Britt Collins <britt.carlin@gmail.com>
2016-04-12 16:02:16 -0600

    yep

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,11 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
-var Portfolio = require('./portfolio');
 var bcrypt = require('bcrypt-nodejs');
 var passportLocalMongoose = require('passport-local-mongoose');
 var SALT_WORK_FACTOR = 10;
 
 var userSchema = new schema({
-	admin: {type: boolean, default: false},
+	admin: {type: Boolean, default: false},
 	username: { type: String, required: true, index: {  sparse: true }, unique: true },
   	password: { type: String, required: true, index: {  sparse: true }, minLength: 7 },
 	firstName: {type: String, lowercase: true, required: true},
@@ -24,8 +23,7 @@ var userSchema = new schema({
 		question8: {type: Number, minlength: 1, maxlength: 5},
 		question9: {type: Number, minlength: 1, maxlength: 5},
 		question10: {type: Number, minlength: 1, maxlength: 5}
-		},
-	portfolio: {type: schema.Types.ObjectId, ref: Portfolio}
+	}
 })
 
 userSchema.plugin(passportLocalMongoose);
