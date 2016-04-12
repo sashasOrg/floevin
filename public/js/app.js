a1bbb093754a617edc96bbf55f3cc1d265a395b5
Britt Collins <britt.carlin@gmail.com>
2016-04-12 15:20:44 -0600

    la

---
+++ public/js/app.js
@@ -0,0 +1,31 @@
+angular.module('SashasApp', ['ui.router'])
+
+angular.module('SashasApp').config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+       url: '/',
+       templateUrl: './views/home.html',
+       controller: 'mainController'
+    })
+    .state('form', {
+      url: '/form',
+      templateUrl: './views/form.html',
+      controller: 'formController'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: './views/login.html',
+      controller: 'authController'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: './views/register.html',
+      controller: 'authController'
+    })
+    .state('portfolio', {
+      url: '/portfolio',
+      templateUrl: './views/portfolio.html',
+      controller: 'userController'
+    })
+    $urlRouterProvider.otherwise('/')
+})
