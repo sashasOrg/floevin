474a3b3675636f377da765f9871f11c72000ffaa
Tanner Andersen <tanner.andersen14@gmail.com>
2016-04-20 09:42:18 -0600

    Got ride of the stocks search, API was broke as hell.

--- src/js/controllers/mainController.js
+++ src/js/controllers/mainController.js
@@ -10,7 +10,7 @@ angular.module('SashasApp').controller('mainController', function($scope, $state
   //   console.log($scope.data)
   // }
   // $scope.getSymbols();
-  
+
   $scope.searchFund = function(symbol) {
     fundService.searchFund(symbol).then(function(response) {
       console.log(response);
@@ -26,15 +26,7 @@ angular.module('SashasApp').controller('mainController', function($scope, $state
   }
   $scope.getFunds();
 
-  $scope.searchSymbols = function() {
-    mainService.searchSymbols($scope.stockQuery).then(function(response) {
-      $cookies.remove('stockData');
-      $cookies.put('stockData', JSON.stringify(response.data.ResultSet.Result));
-      $scope.specificData = JSON.parse($cookies.get('stockData'));
-      $scope.stockQuery = '';
-      $state.reload();
-    })
-  }
+  
   $scope.getMoreInformation = function(symbol) {
     mainService.getMoreInformation(symbol).then(function(response) {
       mainService.specificData = response.data.query.results.quote;
@@ -45,7 +37,6 @@ angular.module('SashasApp').controller('mainController', function($scope, $state
     var user = JSON.parse($cookies.get('currentUser'));
     for (var i = 0; i < user.portfolio.length; i++) {
       if (id === JSON.parse($cookies.get('currentUser')).portfolio[i]) {
-        console.log('Already exists in portfolio');
         return true;
       }
     }
@@ -53,10 +44,8 @@ angular.module('SashasApp').controller('mainController', function($scope, $state
   }
   $scope.addToPortfolio = function(id) {
     var user = JSON.parse($cookies.get('currentUser'));
-    console.log(user.portfolio)
     for (var i = 0; i < user.portfolio.length; i++) {
       if (id === JSON.parse($cookies.get('currentUser')).portfolio[i]) {
-        console.log('Already exists in portfolio');
         return false;
       }
     }
@@ -71,14 +60,12 @@ angular.module('SashasApp').controller('mainController', function($scope, $state
     user = null;
   };
   $scope.removeFromPortfolio = function(id) {
-    console.log('length of thing', JSON.parse($cookies.get('currentUser')).portfolio.length)
     if (JSON.parse($cookies.get('currentUser')).portfolio.length === 1) {
       var user2 = JSON.parse($cookies.get('currentUser'));
       user2.portfolio = [];
       $cookies.remove('currentUser');
       $cookies.put('currentUser', JSON.stringify(user2));
       mainService.updateUser(user2)
-      console.log("hope this works")
       mainService.getUserPortfolio(JSON.parse($cookies.get('currentUser')).username).then(function(response) {
         $cookies.remove('currentUserPortfolio');
         $cookies.put('currentUserPortfolio', JSON.stringify(response.data));
@@ -89,8 +76,6 @@ angular.module('SashasApp').controller('mainController', function($scope, $state
       $cookies.put('currentUserPortfolio', JSON.stringify(response.data));
     })
     for (var i = 0; i < JSON.parse($cookies.get('currentUser')).portfolio.length; i++) {
-      console.log(JSON.parse($cookies.get('currentUser')).portfolio[i])
-      console.log(id);
       if (id === JSON.parse($cookies.get('currentUser')).portfolio[i]) {
         var user = JSON.parse($cookies.get('currentUser'));
         user.portfolio.splice(i, 1);
@@ -98,7 +83,6 @@ angular.module('SashasApp').controller('mainController', function($scope, $state
         $cookies.put('currentUser', JSON.stringify(user));
         mainService.updateUser(user)
         mainService.getUserPortfolio(JSON.parse($cookies.get('currentUser')).username).then(function(response) {
-          console.log('portfolio response here', response);
           $cookies.remove('currentUserPortfolio');
           $cookies.put('currentUserPortfolio', JSON.stringify(response.data));
         })
