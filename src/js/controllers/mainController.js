9ce05156aa7e3c481790d628349bf46e5259486d
ericslc <eric.leatham@gmail.com>
2016-04-22 14:55:43 -0600

    merging updated css/slideshow

--- src/js/controllers/mainController.js
+++ src/js/controllers/mainController.js
@@ -44,4 +44,20 @@ angular.module('SashasApp').controller('mainController', function($scope, $state
   //   })
   // }
   // $scope.getChartInfo();
+/**********************
+image slideshow
+**********************/
+
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  6000);
+
+
 });
